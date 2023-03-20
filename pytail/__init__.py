6fb09fd6c2e05fde0c13aa616b2ae7f95571e617
Gerald <gerald.leikam@aol.com>
2023-03-21 00:47:07 +0100

    Changes to be committed: 	new file:   pytail/VERSION 	modified:   pytail/__init__.py 	modified:   setup.py
     Changes not staged for commit:
    	deleted:    __init__.py

---
+++ pytail/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from codecs import open
+from os import path
+
+try:
+    with open(path.join(path.dirname(__file__), 'VERSION'), encoding='utf-8') as infile:
+        __version__ = infile.read().strip()
+except NameError:
+    __version__ = 'unknown'
+except IOError as ex:
+    __version__ = "unknown (%s)" % ex
+
+from .tail import Tail
--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 from setuptools import setup
 from setuptools import find_packages
+from os.path import abspath
+from os.path import dirname
+from os.path import join
+
+
+here = abspath(dirname(__file__))
+
+with open(join(here, 'pytail', 'VERSION'), encoding='utf-8') as file:
+    version = file.read().strip()
+
 
 setup(
     include_package_data=True,
     name='pytail',
-    version='0.1.0',
+    version=version,
     description='Unix Tail implementation',
     author='Gerald Leikam',
     author_email='gerald.leikam@aol.com',
     url='https://github.com/GeraldLeikam/py-tail',
-    packages=find_packages(),
+    packages=find_packages(exclude=[]),
+    package_data={
+        'pytail': ['VERSION'],
+    },
     long_description='Unix Tail implementation',
     long_description_content_type="text/markdown",
     classifiers=[
